caa184d99fcd91dcee7f9bd63d2013c414d40e1d
Rafał Chełkowski <rchelek@gmail.com>
2020-08-01 22:12:28 +0200

    Poprawiony model książki

--- app/books/models.py
+++ app/books/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 class Book(models.Model):
     book_id = models.CharField(max_length=15, primary_key=True)
-    title = models.CharField(max_length=50)
-    published_date = models.DecimalField(max_digits=4, decimal_places=0)
-    authors = models.ArrayField(models.CharField(max_length=50))
-    average_rating = models.DecimalField(max_digits=2, decimal_places=1)
-    ratings_count = models.PositiveIntegerField()
-    thumbnail_link = models.URLField()
+    title = models.CharField(max_length=50, null=True)
+    categories = models.CharField(max_length=255, null=True)
+    published_date = models.DecimalField(max_digits=4, decimal_places=0, null=True)
+    authors = models.CharField(max_length=255, null=True)
+    average_rating = models.DecimalField(max_digits=2, decimal_places=1, null=True)
+    ratings_count = models.PositiveIntegerField(null=True)
+    thumbnail_link = models.URLField(null=True)
