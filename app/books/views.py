bea5dfd5d83fe3bd717bd70724acfb36219bbba1
Rafał Chełkowski <rchelek@gmail.com>
2020-08-01 18:27:26 +0200

    Initial migrations

--- app/books/views.py
+++ app/books/views.py
@@ -4,3 +4,17 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 # Create your views here.
+
+@require_http_methods(["POST"])
+def get_external_books(request):
+
+    try:
+
+        body = json.loads(request.body.decode('utf-8'))
+
+        new_books = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={body['q']}").json()['items']
+
+
+        return JsonResponse(data=new_books, safe=False)
+    except BaseException as error:
+        return JsonResponse(data={})
